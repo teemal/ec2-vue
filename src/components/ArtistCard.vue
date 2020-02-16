c49052e5929002993ecb04b78770501ee05ba4c8
teemal <taymal1987@gmail.com>
2020-02-15 19:20:10 -0800

    finished nesting components

---
+++ src/components/ArtistCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title">{{name}}</p>
+    </header>
+    <div class="card-content">
+      <div class="content" v-for="(elm, idx) in albs" v-bind:key="idx">
+          <Albums :name="idx" :song="elm"/>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Albums from './AlbumCard'
+export default {
+  props: ["name", "albs"],
+  components: {
+      Albums
+  },
+  data(){
+      return{
+          elm: elm,
+          idx: idx
+      }
+  }
+};
+</script>
+<style scoped>
+</style>
---
+++ src/components/Song.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <img class="play" src="@/assets/play_button.png" alt="" width="40px"><p>{{title}}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: ["title"]
+};
+</script>
+<style scoped>
+.play:hover{
+    cursor: pointer;
+}
+</style>
