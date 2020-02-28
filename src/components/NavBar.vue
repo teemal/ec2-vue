2e7ccacf2097fba910dc767bf4d5e8f909f43206
teemal <taymal1987@gmail.com>
2020-02-27 22:31:13 -0800

    brought over firebase from cloug_auth project

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,30 +1,46 @@
 <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="#">
-      <img src="@/assets/logo.png" height="200" width="70">
-    </a>
-
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-      <a class="navbar-item">
-        SPORKIFY
+    <div class="navbar-brand">
+      <a class="navbar-item" href="#">
+        <img src="@/assets/logo.png" height="200" @click="$router.push('/')"/>
       </a>
+
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item">SPORKIFY</a>
+      </div>
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-light">Log in</a>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
 <script>
-export default {};
+export default {
+    methods: {
+    }
+};
 </script>
+
 <style scoped>
 .navbar{
-    background-color: #148f6c;
-    font-size: 2em;
-}
-.navbar-item{
-    color: white;
+  background-color: aquamarine;
 }
 </style>
