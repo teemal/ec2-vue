e311e02518262060a70d3da0f17944ce7ec3ed1d
teemal <taymal1987@gmail.com>
2020-02-28 20:40:53 -0800

    FINALLY figured out env stuff and updated gitignore

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,55 +1,23 @@
 <template>
   <div class="home">
-    <form action v-on:submit.prevent>
-      <div class="field">
-        <div class="columns">
-          <div class="column is-4 is-offset-4">
-            <input class="input" type="email" placeholder="Email" v-model="email" />
-          </div>
-        </div>
-        <div class="field">
-          <div class="columns">
-            <div class="column is-4 is-offset-4">
-              <input
-                class="input"
-                type="password"
-                placeholder="Password"
-                v-model="password"
-                @keydown.enter="emailLogin()"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+    <div class="columns">
       <div class="columns">
-          <div class="column is-4 is-offset-4">
-              <div class="box has-text-centered">
-          <div class="columns">
-            <div class="column is-3 is-offset-2">
-              <button class="button is-info" v-on:click="goToSignUp()">Sign Up</button>
-            </div>
-            <div class="column is-3 is-offset-2">
-              <button class="button is-success" v-on:click="emailLogin()">Login</button>
-            </div>
-          </div>
-          <div class="columns">
-            <div class="column is-12">
-              <img
-                src="../assets/GoogleSignUpDark.png"
-                alt
-                id="google-signin-button"
-                v-on:click="SignInWithGoogle()"
-              />
-            </div>
-          </div>
+        <div class="column is-6 is-offset-10">
+          <img
+            src="../assets/GoogleSignUpDark.png"
+            alt
+            id="google-signin-button"
+            v-on:click="SignInWithGoogle()"
+          />
         </div>
-          </div>
       </div>
-    </form>
+    </div>
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
+require("dotenv").config();
 // @ is an alias to /src
 /*eslint-disable*/
 export default {
@@ -62,34 +30,10 @@ export default {
     };
   },
   methods: {
-    goToSignUp() {
-      this.$router.push("signup");
-    },
-    emailLogin() {
-      firebase
-        .auth()
-        .signInWithEmailAndPassword(this.email, this.password)
-        .then(res => {
-          // console.log(res.user.uid);
-          var userId = res.user.uid;
-          this.$emit("setUid", userId);
-          this.$router.push("userpage/" + userId);
-        })
-        .catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          if (errorCode === "auth/wrong-password") {
-            alert("Wrong password.");
-          } else {
-            alert(errorMessage);
-          }
-          console.log(error);
-        });
-    },
     SignInWithGoogle() {
+    //   console.log(process.env.VUE_APP_API_KEY);
       var provider = new firebase.auth.GoogleAuthProvider();
-      firebase
+      this.$firebase
         .auth()
         .signInWithPopup(provider)
         .then(res => {
@@ -101,14 +45,15 @@ export default {
           alert(e);
         });
     }
-  }
+  },
+  mounted() {}
 };
 /*eslint-enable*/
 </script>
 
 <style scoped>
 #google-signin-button {
-  width: 80%;
+  width: 90%;
 }
 #google-signin-button:hover {
   cursor: pointer;
@@ -119,7 +64,7 @@ export default {
 .field {
   margin-top: 20px;
 }
-#buttons-box{
-    width: 10vw;
+#buttons-box {
+  width: 10vw;
 }
 </style>
