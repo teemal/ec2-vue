d561f84a72b5b55a60fde0bcfeb5acea6e3f0f9d
teemal <taymal1987@gmail.com>
2020-03-05 18:00:06 -0800

    start playlist  view

---
+++ src/components/PlaylistView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrapper">
+
+  </div>
+</template>
+
+<script>
+// import router from 'vue-router'
+export default {
+  methods: {
+    
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
